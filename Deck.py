444b0ac23fe7fcb2c30dd94c7bd9e4b7c2ac6e03
vidumini <vidumini@m.venusit.tech>
2019-03-27 15:08:14 +0530

    Inheritance - Deck

---
+++ Deck.py
@@ -0,0 +1,13 @@
+from Card import Card;
+
+class Deck:
+    def __init__(self):
+        self.cards = [];
+        for suit in range(4):
+            for rank in range(1, 14):
+                self.cards.append(Card(suit, rank));
+
+    def print_deck(self):
+        for card in self.cards:
+            print(card);
+
